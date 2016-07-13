053c5c5e1bf5d7e3dc4aec6821382503b95caf92
Ricardolv <richardluizv@gmail.com>
2016-07-13 03:38:40 -0300

    Estilizar o segundo componente

--- components/cervejarias/card.vue
+++ components/cervejarias/card.vue
@@ -5,14 +5,20 @@
 </script>
 
 <template>
-    <p>Cervejaria Card!</p>
-
-        <div>
+        <div class="card">
             <h3>{{dados.name}}</h3>
             <p> {{dados.city}} / {{dados.state}}</p>
         </div>
 
 </template>
 
-
-<style></style>
+<style scoped>
+    .card {
+       position: relative;
+       background-color: #f4f4f4;
+       border: #ccc 1px solid;
+       border-radius: 6px;
+       padding: 10px;
+       margin: 10px;
+    }
+</style>
